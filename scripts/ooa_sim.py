e673228c1870e6cf34658c9b87dc9519f09a26cf
Georgia Tsambos <gtsambos@student.unimelb.edu.au>
2022-08-04 09:56:50 +1000

    Small changes

--- scripts/ooa_sim.py
+++ scripts/ooa_sim.py
@@ -12,9 +12,9 @@ import time
 species = stdpopsim.get_species("HomSap")
 model = species.get_demographic_model('OutOfAfricaArchaicAdmixture_5R19')
 graph = model.model.to_demes()
-census_time = 2093.5
+census_time = 500
 # census_time = 50
-num_inds = 100
+num_inds = 50
 
 # # Uncomment to plot
 # _, ax = plt.subplots(1, 1, figsize=(4.5, 4), tight_layout=True)
@@ -33,29 +33,31 @@ ts = msprime.sim_ancestry(
     samples={"YRI": num_inds, "CEU": num_inds, "CHB" : num_inds},
     demography=msp_dem,
     random_seed=1016,
-    sequence_length=51304566, # stdpopsim default for chr22
-    recombination_rate=1.4445e-08 # stdpopsim default for chr1
+    sequence_length=1e6,
+    # sequence_length=51304566, # stdpopsim default for chr22
+    recombination_rate=1.4445e-08, # stdpopsim default for chr1
+    end_time=501
 )
 time_end = time.time()
 print("Total simulation time: ", time_end - time_start)
 
-# # Save the ts in case you want to look at later.
-# ts.dump("ooa.trees")
+# Save the ts in case you want to look at later.
+ts.dump("ooa_50inds_500c-stop501.trees")
 
-# Check there are no coalescences more recent than OOA.
-for t in ts.trees():
-    if t.time(t.root) < census_time:
-        print("Warning: some MRCAs post-date the census time!")
-        break
+# # Check there are no coalescences more recent than OOA.
+# for t in ts.trees():
+#     if t.time(t.root) < census_time:
+#         print("Warning: some MRCAs post-date the census time!")
+#         break
 
-# Run link-ancestors
-print("Timing link_ancestors (ancient census)...")
-time_start = time.time()
-ts.tables.link_ancestors(
-    samples=ts.samples(),
-    ancestors=[u.id for u in ts.nodes() if u.time == census_time])
-time_end = time.time()
-print("Time running link_ancestors (ancient census): ", time_end - time_start)
+# # Run link-ancestors
+# print("Timing link_ancestors (ancient census)...")
+# time_start = time.time()
+# ts.tables.link_ancestors(
+#     samples=ts.samples(),
+#     ancestors=[u.id for u in ts.nodes() if u.time == census_time])
+# time_end = time.time()
+# print("Time running link_ancestors (ancient census): ", time_end - time_start)
 
 # # Run link-ancestors
 # print("Timing link_ancestors (recent census)...")
